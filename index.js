1ccbbbaeccdb0f844b2e2223ba8ef5a6ceabe9f0
lilian njeri ndungu <lilian.ndungu@student.moringaschool.com>
2022-08-23 13:40:21 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+const  returnFirstTwoDrivers = function anonymousFunction([]){
+    return (['Antonia', 'Nuru']);
+    }
+    //return LastTwoDRivers()
+    const  returnLastTwoDrivers = function anonymousFunction([]){
+        return (['Amari', 'Mo']);
+    }
+    const selectingDrivers =[returnFirstTwoDrivers,returnLastTwoDrivers];
+    //createFareMultiplier
+    function createFareMultiplier(anInteger){
+        return function(fare){
+          return fare*anInteger;
+        };
+    }
+    //create fareDoubler(), create fareTripler
+    const fareDoubler = createFareMultiplier(2);
+    const fareTripler = createFareMultiplier(3);
+    const selectDifferentDrivers = function (arrayOfDrivers, driversToReturn) {
+        return driversToReturn(arrayOfDrivers);
+      };
